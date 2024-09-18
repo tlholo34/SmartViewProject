34c081bb8fa5da6e8f5de4d3b43319f8964eb9a3
Tlhologelo <63161730+tlholo34@users.noreply.github.com>
2024-09-19 00:55:14 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum', 'throttle:60,1'])->group(function () {
     Route::get('/meters', [MeterController::class, 'index']);
     Route::post('/meters', [MeterController::class, 'store']);
     Route::get('/meters/{meter}', [MeterController::class, 'show']);
